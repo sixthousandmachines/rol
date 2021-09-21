0c99b2f38dcd8f009ac7f9631df760b50b25a700
Ricky Chun <rchun@fanatics.com>
2021-09-21 18:02:29 -0400

    webpack update

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,8 +12,7 @@ const eslintFormatter = require('react-dev-utils/eslintFormatter')
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
 const paths = require('./paths')
 const getClientEnvironment = require('./env')
-const TerserPlugin = require('terser-webpack-plugin-legacy');
-
+const TerserPlugin = require('terser-webpack-plugin-legacy')
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
